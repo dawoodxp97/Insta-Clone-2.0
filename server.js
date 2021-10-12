baf846e232c97958a44c923dbc3552aed715cd10
Shaik Dawood <77268355+dawoodxp97@users.noreply.github.com>
2021-10-12 11:02:18 +0530

    Fixed issues

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(require("./routes/Auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
