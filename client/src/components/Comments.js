9cca163aa59be9eea4f4d9a229e4da04f2b7767b
Shaik Dawood <77268355+dawoodxp97@users.noreply.github.com>
2021-11-19 06:28:26 +0530

    fixed issues & added new features.

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,27 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/Comments.css";
-import { FiEdit } from "react-icons/fi";
 import { MdDelete } from "react-icons/md";
+import { useStateValue } from "../context/StateProvider";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { EditText } from "react-edit-text";
+import "react-edit-text/dist/index.css";
 
-function Comments({ comments }) {
+toast.configure();
+
+function Comments({ comments, postId }) {
+  const [, dispatch] = useStateValue();
+  const [editText, setEditText] = useState("");
+
   const user = JSON.parse(localStorage.getItem("user"));
+  const successNotify = (text) => {
+    toast.success(text, { autoClose: 1500 });
+  };
+  const warnNotify = (text) => {
+    toast.warn(text);
+  };
+
+  const editComment = (value, id) => {
+    fetch("/editComment", {
+      method: "put",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("auth-token"),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        postId,
+        cID: id,
+        editText: value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        //Comment Added
+        dispatch({
+          type: "SET_RELOAD",
+          reload: Math.floor(Math.random() * 100 + 1),
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        warnNotify("Something went wrong");
+      });
+  };
+
+  const deleteComment = (id) => {
+    fetch("/deleteComment", {
+      method: "put",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("auth-token"),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        postId,
+        cID: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        //Comment Added
+        dispatch({
+          type: "SET_RELOAD",
+          reload: Math.floor(Math.random() * 100 + 1),
+        });
+        successNotify("Comment Deleted");
+      })
+      .catch((err) => {
+        console.log(err);
+        warnNotify("Something went wrong");
+      });
+  };
   return (
     <div className="comments">
       <h4>Comments:</h4>
       <div>
         {comments?.map((c_item) => (
           <div key={c_item?._id}>
-            <p>
-              <strong> {`${c_item?.c_name}:`} </strong> {c_item?.text}
-            </p>
+            {c_item?.postedBy === user?._id ? (
+              <div
+                style={{
+                  width: "40rem",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <p
+                  style={{
+                    width: "8rem",
+                    display: "inline-block",
+                    marginBottom: "0.6rem",
+                  }}
+                >
+                  <strong> {`${c_item?.c_name}:`} </strong>
+                </p>
+                <EditText
+                  style={{ height: "0.5rem" }}
+                  defaultValue={c_item?.text}
+                  onEditMode={() => {
+                    setEditText(c_item?.text);
+                  }}
+                  inline={true}
+                  onSave={(res) => {
+                    editComment(res.value, c_item?._id);
+                  }}
+                />
+              </div>
+            ) : (
+              <p>
+                <strong> {`${c_item?.c_name}:`} </strong> {c_item?.text}
+              </p>
+            )}
             <div>
               {c_item?.postedBy === user?._id ? (
-                <FiEdit className="comment_edit_icon" />
-              ) : (
-                ""
-              )}
-              {c_item?.postedBy === user?._id ? (
-                <MdDelete className="comment_delete_icon" />
+                <MdDelete
+                  onClick={() => {
+                    deleteComment(c_item?._id);
+                  }}
+                  className="comment_delete_icon"
+                />
               ) : (
                 ""
               )}
