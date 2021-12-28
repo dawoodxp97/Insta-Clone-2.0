ad7281d439991ebd6c8145f4176323e58d1e0643
Shaik Dawood <77268355+dawoodxp97@users.noreply.github.com>
2021-12-28 15:57:46 +0530

    added new reducers

--- client/src/context/Reducer.js
+++ client/src/context/Reducer.js
@@ -1,7 +1,9 @@
 export const initialState = {
   allPosts: [],
   reload: null,
-  searchData: null,
+  searchData: [],
+  userDetails: {},
+  userPosts: [],
 };
 
 const reducer = (state, action) => {
@@ -10,6 +12,67 @@ const reducer = (state, action) => {
       return {
         ...state,
         allPosts: action.allPosts,
+      };
+    case "SET_FOLLOWINGS":
+      const followingsUser = { ...state.userDetails };
+      followingsUser.following = action.following;
+      return {
+        ...state,
+        userDetails: followingsUser,
+      };
+    case "SET_LIKES":
+      const mPosts = [...state.allPosts];
+      const index = mPosts.findIndex((obj) => obj._id === action._id);
+      if (index !== -1) {
+        mPosts[index].likes = action.likes;
+      }
+      return {
+        ...state,
+        allPosts: mPosts,
+      };
+    case "SET_COMMENTS":
+      const commPosts = [...state.allPosts];
+      const commIndex = commPosts.findIndex((obj) => obj._id === action._id);
+      if (commIndex !== -1) {
+        commPosts[commIndex].comments = action.comments;
+      }
+      return {
+        ...state,
+        allPosts: commPosts,
+      };
+    case "SET_PIC":
+      const pUser = { ...state.userDetails };
+      pUser.pic = action.pic;
+      return {
+        ...state,
+        userDetails: pUser,
+      };
+    case "DELETE_POST":
+      const dPosts = [...state.userPosts];
+      const dIndex = dPosts.findIndex((obj) => obj._id === action.id);
+      if (dIndex !== -1) {
+        dPosts.splice(dIndex, 1);
+      }
+      return {
+        ...state,
+        userPosts: dPosts,
+      };
+    case "SET_FAVS":
+      let user = { ...state.userDetails };
+      user.favorites = action.favs;
+      return {
+        ...state,
+        userDetails: user,
+      };
+    case "SET_USER":
+      return {
+        ...state,
+        userDetails: action.userDetails,
+      };
+    case "SET_POSTS":
+      return {
+        ...state,
+        userPosts: action.userPosts,
       };
     case "SET_RELOAD":
       return {
@@ -28,3 +91,4 @@ const reducer = (state, action) => {
 };
 
 export default reducer;
+
