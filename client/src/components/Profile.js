3a7135d8a364201a6a6b19779aa36661be2cc568
Shaik Dawood <77268355+dawoodxp97@users.noreply.github.com>
2021-12-28 16:11:49 +0530

    fix: issues

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./styles/Profile.css";
 import ClipLoader from "react-spinners/ClipLoader";
 import { toast } from "react-toastify";
@@ -10,12 +10,8 @@ import { Link } from "react-router-dom";
 toast.configure();
 
 function Profile() {
-  const [{ reload }, dispatch] = useStateValue();
-  const [mypost, setMyposts] = useState([]);
-  const [followers, setFollowers] = useState([]);
-  const [following, setFollowing] = useState([]);
+  const [{ userDetails, userPosts }, dispatch] = useStateValue();
   const [loading, setLoading] = useState(false);
-  const [innerLoad, setInnerLoad] = useState(true);
   const [image, setImage] = useState();
   const [isOpen, setIsOpen] = useState(false);
 
@@ -25,46 +21,20 @@ function Profile() {
   const warnNotify = (text) => {
     toast.warn(text, { autoClose: 1500 });
   };
-
-  useEffect(() => {
-    setFollowers(JSON.parse(localStorage.getItem("user"))?.followers);
-    setFollowing(JSON.parse(localStorage.getItem("user"))?.following);
-  }, []);
-
-  useEffect(() => {
-    let isMount = true;
-    fetch("/myposts", {
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("auth-token"),
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        if (isMount) {
-          setMyposts(result.mypost);
-          setInnerLoad(false);
-        }
-      });
-    return () => {
-      isMount = false;
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [reload]);
   const deletePost = (postID) => {
     setLoading(true);
-    fetch(`/deletepost/${postID}`, {
+    fetch(`/api/post/${postID}`, {
       method: "delete",
       headers: {
-        Authorization: "Bearer " + localStorage.getItem("auth-token"),
+        Authorization: "Bearer " + localStorage.getItem("token"),
       },
     })
       .then((res) => res.json())
       .then((result) => {
         //Post Deleted
         dispatch({
-          type: "SET_RELOAD",
-          reload: Math.floor(Math.random() * 100 + 1),
+          type: "DELETE_POST",
+          id: postID,
         });
         setLoading(false);
         successNotify("Post Deleted Successfully");
@@ -89,10 +59,10 @@ function Profile() {
       .then((res) => res.json())
       .then((data) => {
         if (data?.secure_url) {
-          fetch("/updatepic", {
+          fetch("/api/user", {
             method: "PUT",
             headers: {
-              Authorization: "Bearer " + localStorage.getItem("auth-token"),
+              Authorization: "Bearer " + localStorage.getItem("token"),
               "Content-Type": "application/json",
             },
             body: JSON.stringify({
@@ -101,11 +71,12 @@ function Profile() {
           })
             .then((res) => res.json())
             .then((result) => {
-              setLoading(false);
+              console.log(result);
               dispatch({
-                type: "SET_RELOAD",
-                reload: Math.floor(Math.random() * 100 + 1),
+                type: "SET_PIC",
+                pic: result?.pic,
               });
+              setLoading(false);
             })
             .catch((err) => {
               setLoading(false);
@@ -118,23 +89,6 @@ function Profile() {
         console.log(err);
       });
   };
-  useEffect(() => {
-    fetch("/currUser", {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("auth-token"),
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        localStorage.setItem("user", JSON.stringify(result?.user));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [reload]);
 
   return (
     <div className="profile">
@@ -151,7 +105,9 @@ function Profile() {
             <div className="create_post">
               <form onSubmit={handleSubmit}>
                 <label className="upload_btn">
-                  {!image ? "Upload Image" : `Selected: ${image.name}`}
+                  {!image
+                    ? "Upload Image"
+                    : `Selected: ${image.name.substring(0, 7)}...`}
                   <input
                     onChange={(e) => setImage(e.target.files[0])}
                     className="modal_file"
@@ -166,7 +122,7 @@ function Profile() {
       )}
       <div className="profile_1">
         <div className="profile_1_child1">
-          <img src={JSON.parse(localStorage.getItem("user"))?.pic} alt="" />
+          <img src={userDetails?.pic} alt="" />
           <div
             onClick={() => {
               setIsOpen(true);
@@ -178,12 +134,12 @@ function Profile() {
         </div>
         <div className="profile_1_child2">
           <div className="profile_details1">
-            <h2>{JSON.parse(localStorage.getItem("user"))?.name}</h2>{" "}
+            <h2>{userDetails?.name}</h2>{" "}
           </div>
           <div className="profile_details2">
-            <span>{`${mypost.length} Posts`}</span>
-            <span>{`${followers.length} Followers`}</span>
-            <span>{`${following.length} Following`}</span>
+            <span>{`${userPosts?.length} Posts`}</span>
+            <span>{`${userDetails?.followers?.length} Followers`}</span>
+            <span>{`${userDetails?.following?.length} Following`}</span>
           </div>
           <div className="profile_details3"></div>
         </div>
@@ -191,12 +147,11 @@ function Profile() {
       <div>
         <h3 className="user_posts_title">My Posts</h3>
       </div>
-      <div className="inner_load">
-        <ClipLoader color="#fe5656" loading={innerLoad} size={50} />
-      </div>
+
       <div className="profile_2">
-        {mypost &&
-          mypost.map((item) => (
+        {userPosts.length === 0 ? <p>No Posts yet</p> : ""}
+        {userPosts &&
+          userPosts.map((item) => (
             <div key={item?._id} className="profile_posts">
               {item?.postedBy?._id ===
               JSON.parse(localStorage.getItem("user"))?._id ? (
@@ -218,7 +173,7 @@ function Profile() {
                         textDecoration: "none",
                         color: "#ffffff",
                       }}
-                      to={`/home/post/${item?._id}`}
+                      to={`/postDetails/${item?._id}`}
                     >
                       View
                     </Link>
@@ -227,7 +182,6 @@ function Profile() {
               ) : (
                 ""
               )}
-
               <img src={item?.photo} alt={item?.message} />
             </div>
           ))}
