4faf19ccc58e5adc165232e59febe7c034afa975
Shaik Dawood <77268355+dawoodxp97@users.noreply.github.com>
2021-12-28 15:59:46 +0530

    fix: issues

--- client/src/components/Favorites.js
+++ client/src/components/Favorites.js
@@ -1,243 +1,50 @@
-import React, { useEffect, useState } from "react";
-import CopyToClipboard from "react-copy-to-clipboard";
-import { BiShareAlt } from "react-icons/bi";
-import { HiHeart, HiOutlineHeart } from "react-icons/hi";
-import { RiChat1Line } from "react-icons/ri";
-import { Link } from "react-router-dom";
+import React from "react";
 import { useStateValue } from "../context/StateProvider";
 import "./styles/Favorites.css";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
-import ClipLoader from "react-spinners/ClipLoader";
+import { Link } from "react-router-dom";
 
 toast.configure();
 
 function Favorites() {
-  const [{ reload }, dispatch] = useStateValue();
-  const [favData, setFavData] = useState([]);
-  const [innerLoad, setInnerLoad] = useState(true);
-
-  const addToFav = (id) => {
-    fetch("/addfav", {
-      method: "put",
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("auth-token"),
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        postID: id,
-      }),
-    })
-      .then(async (response) => {
-        try {
-          const data = await response.json();
-          localStorage.setItem("user", JSON.stringify(data));
-          dispatch({
-            type: "SET_RELOAD",
-            reload: Math.floor(Math.random() * 100 + 1),
-          });
-        } catch (error) {
-          console.log("Error happened here!");
-          console.error(error);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  const deleteFav = (id) => {
-    fetch("/deletefav", {
-      method: "put",
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("auth-token"),
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        postID: id,
-      }),
-    })
-      .then(async (response) => {
-        try {
-          const data = await response.json();
-          localStorage.setItem("user", JSON.stringify(data));
-          dispatch({
-            type: "SET_RELOAD",
-            reload: Math.floor(Math.random() * 100 + 1),
-          });
-        } catch (error) {
-          console.log("Error happened here!");
-          console.error(error);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  useEffect(() => {
-    fetch("/currUser", {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("auth-token"),
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        setFavData(result?.user?.favorites);
-        setInnerLoad(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [reload]);
-
-  const successNotify = (text) => {
-    toast.success(text, { autoClose: 1500 });
-  };
+  const [{ userDetails }] = useStateValue();
   return (
-    <div className="fav_container">
-      <div className="fav_grp">
-        <h2>Favorites</h2>
-        {favData.length === 0 ? (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-              height: "90vh",
-              width: "80%",
-            }}
-          >
-            <ClipLoader color="#fe5656" loading={innerLoad} size={50} />
+    <div className="fav_grp">
+      <h2>Favorites</h2>
+      {userDetails.favorites?.length === 0 ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            height: "90vh",
+            width: "80%",
+          }}
+        >
+          <p>No Favorites Added</p>
+        </div>
+      ) : (
+        <div className="favs">
+          <div className="profile_2">
+            {userDetails.favorites &&
+              userDetails.favorites.map((item) => (
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: "#121212",
+                  }}
+                  to={`/postDetails/${item?._id}`}
+                >
+                  <div key={item?._id} className="profile_posts">
+                    <img src={item?.photo} alt={item?.message} />
+                  </div>
+                </Link>
+              ))}
           </div>
-        ) : (
-          ""
-        )}
-        {favData &&
-          favData.map((item) => (
-            <div key={item?._id} style={{ width: "80%" }} className="post">
-              <Link
-                style={{
-                  textDecoration: "none",
-                  color: "#121212",
-                }}
-                to={`/home/post/${item?._id}`}
-              >
-                <div className="post_body fav_post">
-                  <p>{item?.message}</p>
-                  <img src={item?.photo} alt="" />
-                </div>
-              </Link>
-              <div className="post_footer">
-                <div className="post_footer_like">
-                  {item?.likes.includes(
-                    JSON.parse(localStorage.getItem("user"))?._id
-                  ) ? (
-                    <HiHeart
-                      onClick={() => {
-                        fetch("/unlike", {
-                          method: "put",
-                          headers: {
-                            Authorization:
-                              "Bearer " + localStorage.getItem("auth-token"),
-                            "Content-Type": "application/json",
-                          },
-                          body: JSON.stringify({
-                            postId: item?._id,
-                          }),
-                        })
-                          .then((res) => res.json())
-                          .then((result) => {
-                            dispatch({
-                              type: "SET_RELOAD",
-                              reload: Math.floor(Math.random() * 100 + 1),
-                            });
-                          })
-                          .catch((err) => console.log(err));
-                      }}
-                      className="like_icon"
-                    />
-                  ) : (
-                    <HiOutlineHeart
-                      onClick={() => {
-                        fetch("/like", {
-                          method: "put",
-                          headers: {
-                            Authorization:
-                              "Bearer " + localStorage.getItem("auth-token"),
-                            "Content-Type": "application/json",
-                          },
-                          body: JSON.stringify({
-                            postId: item?._id,
-                          }),
-                        })
-                          .then((res) => res.json())
-                          .then((result) => {
-                            dispatch({
-                              type: "SET_RELOAD",
-                              reload: Math.floor(Math.random() * 100 + 1),
-                            });
-                          })
-                          .catch((err) => console.log(err));
-                      }}
-                      className="unlike_icon"
-                    />
-                  )}
-
-                  <span>{`${item?.likes.length} Likes`}</span>
-                </div>
-                <div className="post_footer_comment">
-                  <RiChat1Line />
-                  <span>{`${item?.comments.length} Comments`}</span>
-                </div>
-                <div className="post_footer_share">
-                  <BiShareAlt />
-                  <CopyToClipboard
-                    text={`https://ins-skd.herokuapp.com/home/post/${item?._id}`}
-                    onCopy={() => {
-                      successNotify("Link Copied to Clipboard");
-                    }}
-                  >
-                    <span style={{ cursor: "pointer" }}>Share</span>
-                  </CopyToClipboard>
-                </div>
-                {JSON.parse(localStorage.getItem("user")).favorites?.some(
-                  function (elem) {
-                    return elem._id === item?._id;
-                  }
-                ) ? (
-                  <div className="post_footer_saved">
-                    <BsBookmarkFill />
-                    <span
-                      onClick={() => {
-                        deleteFav(item?._id);
-                      }}
-                      style={{ cursor: "pointer" }}
-                    >
-                      Saved
-                    </span>
-                  </div>
-                ) : (
-                  <div className="post_footer_saved">
-                    <BsBookmark />
-                    <span
-                      onClick={() => {
-                        addToFav(item?._id);
-                      }}
-                      style={{ cursor: "pointer" }}
-                    >
-                      Save
-                    </span>
-                  </div>
-                )}
-              </div>
-            </div>
-          ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
