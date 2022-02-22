4922a187994870941d181c0102517f754948e9ad
Shaik Dawood <77268355+dawoodxp97@users.noreply.github.com>
2022-02-22 14:16:52 +0530

    fix:issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import Register from "./components/Register";
+import Layout from "./components/Layout";
+import Feed from "./components/Feed";
+import Profile from "./components/Profile";
+import Favorites from "./components/Favorites";
+import PostDetails from "./components/PostDetails";
+import UsersProfile from "./components/Usersprofile";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/home">
-            <Header />
-            <Home />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/">
-            <SignIn />
-          </Route>
+          <Route exact path="/" component={SignIn} />
+          <Route path="/register" component={Register} />
+          <Layout>
+            <Route path="/home" component={Feed} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/favorites" component={Favorites} />
+            <Route path="/postDetails/:postID" component={PostDetails} />
+            <Route path="/userProfile/:userID" component={UsersProfile} />
+          </Layout>
         </Switch>
       </Router>
     </div>
@@ -28,3 +31,4 @@ function App() {
 }
 
 export default App;
+
